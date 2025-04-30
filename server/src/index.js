8b741d2f990671316a96615f417024fe0dcab0d8
Mohammedhasmi-Patel <patelmohammedhasmi@gmail.com>
2025-04-30 23:22:05 +0530

    create db connection code

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,9 @@
-import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/authRoute.js";
-
 dotenv.config();
+
+import express from "express";
+import authRoutes from "./routes/authRoute.js";
+import conn from "./utils/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,5 +11,6 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
+  conn();
   console.log("Server is running on port 8080");
 });
